b0b1b1ccf33059035d21c5e5681814d604c4ec37
navy <arenas@aliyun.com>
2024-01-09 23:48:10 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -8,6 +8,10 @@ import vue from '@vitejs/plugin-vue';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/leaderboard-frontend",
+  build: {
+    outDir: "dist"
+  },
   plugins: [
     vue(),
     AutoImport({
